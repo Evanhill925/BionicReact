41ecf595ae9cb34eb87299b3565f1b39d0b7ab0a
Evan <evanhill925@gmail.com>
2023-09-14 15:35:23 -0700

    Pushed the backend home route

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -13,4 +13,13 @@ router.get("/gallery", async (req, res) => {
   }
 })
 
+router.get("/home", async (req, res) => {
+  const entry = schemas.Entry
+  const pulledFromDb = await entry.find({}).exec()
+  let heldItems = pulledFromDb.slice(-6).reverse()
+  if (heldItems) {
+    res.send(JSON.stringify(heldItems))
+  }
+})
+
 module.exports = router
