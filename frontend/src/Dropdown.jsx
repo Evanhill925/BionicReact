93be4d7ef49a9ccdf141c7cbdf4dfa84909ec25d
root <youremail@domain.com>
2023-10-21 10:14:26 -0700

    disabled dalle 3

--- frontend/src/Dropdown.jsx
+++ frontend/src/Dropdown.jsx
@@ -12,10 +12,10 @@ function DropdownMenu({ onOptionSelect }) {
       <h1>Dropdown Menu Example</h1>
       <select onChange={handleOptionChange}>
         <option value="">Midjourney (recommended)</option>
-        <option value="--niji 5">niji </option>
-        <option value="--niji 5 --style cute">niji cute</option>
-        <option value="--niji 5 --style scenic">niji scenic</option>
-        <option value="--niji 5 --style expressive">niji expressive</option>
+        <option value=" --niji 5">niji </option>
+        <option value=" --niji 5 --style cute">niji cute</option>
+        <option value=" --niji 5 --style scenic">niji scenic</option>
+        <option value=" --niji 5 --style expressive">niji expressive</option>
         {/* <option value="Dalle 3">Dalle 3</option> */}
 
       </select>
