18e2d0d74de9d084296ab6383e50700ca61f3ded
root <youremail@domain.com>
2025-05-02 18:44:19 -0700

    expanded express to handle 50mb uploads

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,8 @@ const router = require("./routes/router")
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 const corsOptions = {
   origin: "*",
   Credentials: true,
