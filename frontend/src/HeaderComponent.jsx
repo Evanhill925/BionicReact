a08c6bac745094f30d7ed226a5010b4048b23125
Evan <evanhill925@gmail.com>
2025-05-20 12:47:03 -0700

    updated header component

--- frontend/src/HeaderComponent.jsx
+++ frontend/src/HeaderComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 
 function HelmetComponent({ imageDict }) {
     const [url, setUrl] = useState('');
