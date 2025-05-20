b87313666f648fd3c8495961b8706be58ee09f92
Evan <evanhill925@gmail.com>
2025-05-20 12:31:24 -0700

    switched from react-helmet-async

--- frontend/src/components/SEO.jsx
+++ frontend/src/components/SEO.jsx
@@ -1,6 +1,6 @@
 // components/SEO.js
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 import PropTypes from 'prop-types'; // For prop validation
 import logo from "../crlogo.png"
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client"
 import { RouterProvider } from "react-router-dom"
 import { router } from "./Router.jsx"
 import { ThemeProvider } from "./ThemeContext"
+import { HelmetProvider } from 'react-helmet-async';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "./custom-theme.css"
 import "./App.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <HelmetProvider>
     <ThemeProvider>
       <RouterProvider router={router} />
     </ThemeProvider>
+    </HelmetProvider>
   </React.StrictMode>
 )
